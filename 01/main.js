accacd119dae6fcfc89e233066586484c218d9f3
bsdimen <nimene505@gmail.com>
2024-10-08 21:32:55 +0100

    adding new section

--- 01/main.js
+++ 01/main.js
@@ -1,4 +1,4 @@
-/*===============Menu====================*/
+/*=============================Menu==============================*/
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.querySelector("#menu_bar");
     const menu = document.querySelector("#menu_links");
@@ -11,8 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-/*===============Content====================*/
-
+/*=========================Hero-Content==========================*/
 window.addEventListener('keydown', (event) => {
     const para = document.querySelector("#hero_para");
     const heading = document.querySelector("#hero_h1");
@@ -22,4 +21,26 @@ window.addEventListener('keydown', (event) => {
         para.style.transform = "translateY(-180px)";
         para.style.opacity = "1";
     }
+});
+/*=========================Hisory-Content==========================*/
+document.addEventListener("DOMContentLoaded", () => {
+    const dates = document.querySelectorAll(".line-v");
+    const content = document.querySelectorAll(".event");
+
+    dates.forEach((date, index) => {
+
+        date.addEventListener("click", () => {
+
+            dates.forEach((d) => {
+                d.classList.remove("active");
+            });
+
+            content.forEach((box) => {
+                box.classList.remove("active");
+            });
+            content[index].classList.add("active");
+            date.classList.add("active");
+
+        });
+    });
 });
